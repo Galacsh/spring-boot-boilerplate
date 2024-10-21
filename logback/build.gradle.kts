d4c2ef537082ca31da37219c9fc852dfba75d377
Galacsh <galacsh.dev@gmail.com>
2024-10-21 18:46:18 +0900

    chore: add logback boilerplate

---
+++ logback/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency)
+}
+
+group = "io.galacsh"
+version = "1.0.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
