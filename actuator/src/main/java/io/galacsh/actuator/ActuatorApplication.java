a611f477c62784f5ae56fbb3c8fca6ad7f523bc0
Galacsh <galacsh.dev@gmail.com>
2024-10-22 12:56:24 +0900

    feat: boilerplate using actuator

---
+++ actuator/src/main/java/io/galacsh/actuator/ActuatorApplication.java
@@ -0,0 +1,15 @@
+package io.galacsh.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
---
+++ actuator/src/main/java/io/galacsh/actuator/AppRunner.java
@@ -0,0 +1,20 @@
+package io.galacsh.actuator;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("""
+                ================================================================================
+                See difference when running the application with the "prod" profile.
+                
+                👍 Checkout this URL for more information about the actuator endpoints:
+                    https://docs.spring.io/spring-boot/docs/current/reference/html/actuator.html
+                ================================================================================
+                """);
+    }
+}
